d3388d2c3d96b6af40d9265186741f1d4c61b968
Ubuntu <hungiscoolguy@yahoo.com>
2021-01-15 22:55:43 -0800

    updated

--- Lab3_ex2/source/main.c
+++ Lab3_ex2/source/main.c
@@ -14,10 +14,16 @@
 
 int main(void) {
     /* Insert DDR and PORT initializations */
-
+	DDRA = 0x00; PORTA = 0xFF; // Configure port A's  pins as inputs
+	DDRC = 0xFF; PORTC = 0x00;
+	unsigned char tmpA = 0x00;
+	unsigned char val = 0x00;
     /* Insert your solution below */
     while (1) {
-
+	tmpA = PINA;
+	if(tmpA == 1 || tmpA == 2){
+		PORTC = 0x20; //100000 sets PC5 to on;
+	}
     }
     return 1;
 }
