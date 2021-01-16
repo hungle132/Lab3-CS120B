26cf34c62080d25cb7c65b9fabcb8cf0d40c6b30
Ubuntu <hungiscoolguy@yahoo.com>
2021-01-15 23:45:22 -0800

    updated

--- Lab3_ex2/source/main.c
+++ Lab3_ex2/source/main.c
@@ -17,12 +17,31 @@ int main(void) {
 	DDRA = 0x00; PORTA = 0xFF; // Configure port A's  pins as inputs
 	DDRC = 0xFF; PORTC = 0x00;
 	unsigned char tmpA = 0x00;
-	unsigned char val = 0x00;
+	unsigned char lessthan = 0x00;
     /* Insert your solution below */
     while (1) {
 	tmpA = PINA;
 	if(tmpA == 1 || tmpA == 2){
 		PORTC = 0x20; //100000 sets PC5 to on;
+	}
+	else if (tmpA == 3 || tmpA == 4){
+	
+	}
+	else if (tmpA == 5 || tmpA == 6){
+	
+	}
+	else if (tmpA == 7 || tmpA == 8 || tmpA == 9){
+
+	}
+	else if (tmpA == 10 || tmpA == 11 || tmpA == 12){
+
+	}
+	else if (tmpA == 13 || tmpA == 14 || tmpA == 15){
+
+	}
+	if (tmpA <= 4){
+		lessthan = 0x40;
+		PORTC = tmpA + lessthan;
 	}
     }
     return 1;
