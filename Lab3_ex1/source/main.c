10527be361419434839d22805614d6add5a878fd
Ubuntu <hungiscoolguy@yahoo.com>
2021-01-15 21:36:52 -0800

    Updated

--- Lab3_ex1/source/main.c
+++ Lab3_ex1/source/main.c
@@ -15,9 +15,26 @@
 int main(void) {
     /* Insert DDR and PORT initializations */
 DDRA = 0x00; PORTA = 0xFF; // Configure port A's  pins as inputs
-	DDRC = 0xFF; PORTC = 0x00;
+DDRB = 0x00; PORTB = 0xFF; // port B as inputs;
+DDRC = 0xFF; PORTC = 0x00;
+
+unsigned char tmpA = 0x00;
+unsigned char tmpB = 0x00;
+unsigned char val = 0x00;
     /* Insert your solution below */
     while (1) {
+	
+	for(unsigned int i = 0; i < 8 ; i++){
+		tmpA = GetBit(PINA,i);
+		if(tmpA == 1){
+		val = val + 1;
+		}
+		tmpB = GetBit(PINB,i);
+		if(tmpB == 1){
+		val = val + 1;
+		}	
+	}
+	PORTC = val;
 
     }
     return 1;
